eb2c6e0f6a4d20bdfd2be9b9c4ff8ea9286c837a
mateusjdev <mateusjuniordev@gmail.com>
2020-10-19 17:03:28 -0300

    Add -h help argument Add -H HASH argument

--- README.md
+++ README.md
@@ -7,16 +7,23 @@
 The file is written in the language `python` which means you have to have `python` installed.
 If not: Do so [here](https://www.python.org/).
 
-To run the script open a command prompt/shell and type in: `python !rename.py`
-This will rename all files (itself excluded) to the hashing algorithm `md5`.
-
-If you want to use another hashing algorithm from the list use this command:
-`python rename.py [md5/sha1/sha224/sha256/sha384/sha512]`
+To run the script open a command prompt/shell and type in: `python rename.py`
+This will rename all files to the selected hashing algorithm (`md5` is default).
 
 #### Supported hashes
-1. MD5
-2. SHA1
-3. sha224
-4. sha256
-5. sha384
-6. sha512
+```
+md5 / sha1 / sha224 / sha256 / sha384 / sha512
+```
+
+#### Usage
+```
+Usage: rename.py [OPTIONS] 
+
+Options:
+  -h            Print this help text and exit
+  -H HASH       Hash that will be used: [md5/sha1/sha224/sha256/sha384/sha512]
+  -i DIR/FILE   (Soon) Files that will be hashed
+  -o DIR        (Soon) Location were hashed files will be stored
+  -r            (Soon) Recursive
+  -s FILE       (Soon) Saves a log in .json format
+```
--- rename.py
+++ rename.py
@@ -1,6 +1,17 @@
 import os
 import sys
 import hashlib
+import getopt
+
+def help():
+	print('Usage: rename.py [OPTIONS]\n')
+	print('Options:')
+	print('  -h           Print this help text and exit')
+	print('  -H HASH      Hash that will be used: [md5/sha1/sha224/sha256/sha384/sha512]')
+	print('  -i DIR/FILE  (Soon) Files that will be hashed')
+	print('  -o DIR       (Soon) Location were hashed files will be stored')
+	print('  -r           (Soon) Recursive')
+	print('  -s FILE      (Soon) Saves a log in .json format')
 
 def hash(file, method):
 	if not os.path.isdir(file):
@@ -16,41 +27,62 @@ def hash(file, method):
 			sum = hashlib.sha384(f.read()).hexdigest()
 		elif method == "sha512":
 			sum = hashlib.sha512(f.read()).hexdigest()
-		elif method == "md5":
-			sum = hashlib.md5(f.read()).hexdigest()
+		else: # md5
+			sum = hashlib.md5(f.read()).hexdigest() 
 		f.close()
 		return sum
 	else:
 		return "dir"
 
-unsorted = os.listdir('.')
-filelist = sorted(unsorted, key=len)
+def main(argv):
 
-for file in filelist:
-	if not file == "rename.py":
+	use_hash = ''
 
-		filename, file_extension = os.path.splitext(file)
+	try:
+		opts, args = getopt.getopt(argv,"hH:",["hash="])
+	except getopt.GetoptError:
+		help()
+		sys.exit(1)
 
-		if len(sys.argv) >= 2:
-			sum = hash(file, sys.argv[1])
-		else:
-			sum = hash(file, "md5")
-		try:
-			print("Trying to rename: " + file)
-
-			if os.path.isfile(sum + file_extension):
-				print("file " + sum + file_extension + " already exists")
-				if not file == (sum + file_extension):
-					print("Removing: " + file + " because it is a duplicate")
-					os.remove(file)
-
-			elif not sum == "dir":
-				os.rename(file, sum + file_extension)
-				print(file + ' --> ' + sum + file_extension)
+	for opt, arg in opts:
+		if opt == "-h":
+			help()
+			sys.exit(1)
+		elif opt in ("-H", "--hash"):
+			if(arg == "sha1" or arg == "sha224" or arg == "sha256" or arg == "sha384" or arg == "sha512" or arg == "md5"):
+				use_hash = arg
 			else:
-				print("Skipping directory " + file)
-		except Exception as e:
-			print("Error displaying file name.")
+				print("rename.py: invalid option \"-H " + arg + "\"\n")
+				help()
+				sys.exit(1)
 
-	print("")
-input("Press any key to exit...")
+	unsorted = os.listdir('.')
+	filelist = sorted(unsorted, key=len)
+
+	for file in filelist:
+		if not file == "rename.py":
+			filename, file_extension = os.path.splitext(file)
+
+			sum = hash(file, use_hash)
+
+			try:
+				print("Trying to rename: " + file)
+
+				if os.path.isfile(sum + file_extension):
+					print("file " + sum + file_extension + " already exists")
+					if not file == (sum + file_extension):
+						print("Removing: " + file + " because it is a duplicate")
+						os.remove(file)
+				elif not sum == "dir":
+					os.rename(file, sum + file_extension)
+					print(file + ' --> ' + sum + file_extension)
+				else:
+					print("Skipping directory " + file)
+			except Exception as e:
+				print("Error displaying file name.")
+
+		print("")
+	x = raw_input("Press any key to exit...")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
