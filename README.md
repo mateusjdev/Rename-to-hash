7b720e1c502b036c90af0bb2157e2fe72b0a9571
mateusjdev <mateusjuniordev@gmail.com>
2024-03-18 18:59:16 -0300

    Code cleaning

--- README.md
+++ README.md
@@ -1,57 +1,44 @@
-# Rename-Files-To-Hash
+# Rename Files to Hash
 
 Single python file to rename all files in a directory to their hash sums
 
-## Run the file
+## Usage
 
-The file is written in the language `python` which means you have to have `python` installed.
-If not: Do so [here](https://www.python.org/).
+The file is written in the language `python` which means you have to have `python` installed. If not, do so [here](https://www.python.org/).
 
 To run the script open a command prompt/shell and type in: `python rename.py`
 This will rename all files to the selected hashing algorithm (`md5` is default).
 
-## Supported hashes
-
-```txt
-md5 / sha1 / sha224 / sha256 / sha384 / sha512
-```
-
 > :warning: **Hashsum libraries can have collisions**: Some hashed files can be diferent and have the same checksum causing one of them to be deleted. You can learn an example of MD5 collision [here](https://www.mscs.dal.ca/~selinger/md5collision/).
 
-## Usage
+## Options
 
 ```txt
-Usage: rename.py [OPTIONS]
-
-Options:
-  -h            Print this help text and exit
-  -d            Dry run, doesn't rename or delete files
-  -H HASH       Hash that will be used: [md5/sha1/sha224/sha256/sha384/sha512]
-  -i DIR/FILE   Files that will be hashed
-  -o DIR        Location were hashed files will be stored
+-h            Print this help text and exit
+-d            Dry run, doesn't rename files
+-H HASH       Hash that will be used: [md5/sha1/sha224/sha256/sha384/sha512]
+-i DIR/FILE   Files that will be hashed
+-o DIR        Location were hashed files will be stored
 ```
 
-## todo
+**Supported hashes:** `md5 / sha1 / sha224 / sha256 / sha384 / sha512`
 
-- apply common path to logs
+## TODO
+- Print only common path to logs
+- Add -r/--recursive
+- Add multiple -i/--inputs || Fix multiple -i/--inputs
+- Normalize file extensions (.MP4 -> .mp4)
+- Check if in .git repo
+- Add blake2b and blake3
+- Remove sha1, sha224, sha384
 
-```python
-# solve:
-# input -> hash
-# A -> B
-# B -> A
-# exec $0
-# A -> B_1
-# B -> A
-```
+## Possible Improvements
 
-## Maybe
+These options can be implemented easily, but in my use case, they are not relevant
 
-These options can be implemented easily, but in my use case, they are not needed
-
-- "--delete": As now, due to hash colision, duplicated files are not deleted
-- "--confirm": Use another hash to compare duplicated files
-- "--preserve": Location to move and preserve duplicated files
-- "--input": Add more than 1 dir
+- Colision related
+  - "--delete": As now, due to hash colision, duplicated files are not deleted
+  - "--confirm": Use another hash to compare duplicated files
+  - "--preserve": Location to move and preserve duplicated files
 - "--json": save logs/every action with json
 - DEBUG: Create tests
--- rename.py
+++ rename.py
@@ -3,20 +3,22 @@ import sys
 import hashlib
 import argparse
 
-USER_ERROR = 2
-CODE_ERROR = 3
-VERSION = "v2.0rc"
+USER_ERROR = 3
+CODE_ERROR = 4
+
+VERSION = "v2.1.1"
+
 
 def exit_with_error(error: str, err_nu: int):
     print("ERROR: " + error)
     sys.exit(err_nu)
 
 
-def hash_file(file_path: str, hash_method: str) -> str:
+def hash_file(file_path: str, algorithm: str) -> str:
     if os.path.isdir(file_path):
         exit_with_error("Not a valid file:" + file_path, USER_ERROR)
 
-    dict_hash_methods = {
+    dict_algorithm = {
         "md5": hashlib.md5,
         "sha1": hashlib.sha1,
         "sha224": hashlib.sha224,
@@ -26,55 +28,35 @@ def hash_file(file_path: str, hash_method: str) -> str:
     }
 
     try:
-        hash = dict_hash_methods[hash_method]()
+        hashing = dict_algorithm[algorithm]()
     except KeyError:
-        exit_with_error("Could't compute file hash from" + file_path, CODE_ERROR)
+        exit_with_error("Could't compute file hash from" +
+                        file_path, CODE_ERROR)
 
     with open(file_path, 'rb') as file_bin:
         for block in iter(lambda: file_bin.read(4096), b''):
-            hash.update(block)
-        
-    return hash.hexdigest()
+            hashing.update(block)
+
+    return hashing.hexdigest()
 
 
-def valid_hash(string):
-    hashs = ["sha1", "sha224", "sha256", "sha384", "sha512", "md5"]
-    if string in hashs:
-        return string
-    exit_with_error("Not a valid hash algorithm" + string, USER_ERROR)
+def is_path(path: str):
+    abs_path = os.path.abspath(path)
+    if os.path.isfile(abs_path) or os.path.isdir(abs_path):
+        return abs_path
+
+    exit_with_error('No such file or directory: ' + path, USER_ERROR)
 
 
-def is_path(string):
-    abs_arg = os.path.abspath(string)
-    if os.path.isfile(abs_arg) or os.path.isdir(abs_arg):
-        return abs_arg
-    exit_with_error('No such file or directory: ' + string, USER_ERROR)
+def is_dir(path: str):
+    abs_path = os.path.abspath(path)
+    if os.path.isdir(abs_path):
+        return abs_path
+
+    exit_with_error('Not a valid directory: ' + path, USER_ERROR)
 
 
-def is_folder(string):
-    abs_arg = os.path.abspath(string)
-    if not os.path.isdir(abs_arg):
-        exit_with_error('Not a valid directory: ' + string, USER_ERROR)
-    return abs_arg
-
-
-def can_be_saved(string):
-    abs_arg = os.path.abspath(string)
-    if os.path.isdir(abs_arg):
-        exit_with_error('Not a valid filename: ' + string, USER_ERROR)
-    elif os.path.isfile(abs_arg):
-        exit_with_error('File already exists: ' + string, USER_ERROR)
-    elif not os.path.exists(abs_arg) and os.path.isdir(os.path.dirname(abs_arg)):
-        return abs_arg
-    else:
-        exit_with_error('Not a valid path: ' + string, USER_ERROR)
-
-
-def _action_move(source: str, destination: str, dry_run: bool):
-    # todo: get common base path from source and destination
-    # example: linux mv -v
-    #   if commom path, show input.txt -> output.txt
-    # todo: check if source and destination are valid
+def action_move(source: str, destination: str, dry_run: bool):
     if dry_run:
         print('(dry-run) ' + source + ' --> ' + destination)
     else:
@@ -82,18 +64,18 @@ def _action_move(source: str, destination: str, dry_run: bool):
         print(source + ' --> ' + destination)
 
 
-def action_move(source: str, destination: str, dry_run: bool):
+def try_move(source: str, destination: str, dry_run: bool):
 
     if not os.path.isfile(source):
         exit_with_error("Cannot move dir", CODE_ERROR)
 
     if os.path.isdir(destination):
         destination = destination + "/" + os.path.basename(source)
-        action_move(source, destination, dry_run)
+        try_move(source, destination, dry_run)
         return
 
     if not os.path.exists(destination):
-        _action_move(source, destination, dry_run)
+        action_move(source, destination, dry_run)
         return
 
     if os.path.samefile(source, destination):
@@ -111,13 +93,13 @@ def action_move(source: str, destination: str, dry_run: bool):
                 return
             iterator = iterator + 1
         else:
-            _action_move(source, new_destination, dry_run)
+            action_move(source, new_destination, dry_run)
             return
-
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Single python file to rename all files in a directory to their hash sums.")
+        description="Single python file to rename all files in a directory to \
+            their hash sums.")
 
     parser.add_argument('-d',
                         '--dry-run',
@@ -126,9 +108,12 @@ def main():
 
     parser.add_argument('-H',
                         '--hash',
-                        type=valid_hash,
                         default='md5',
-                        help='hash that will be used: [md5/sha1/sha224/sha256/sha384/sha512]')
+                        choices=["md5", "sha1", "sha224", "sha256",
+                                 "sha384", "sha512"],
+                        metavar="HASH",
+                        help='hash that will be used: \
+                        [md5/sha1/sha224/sha256/sha384/sha512]')
 
     parser.add_argument('-i',
                         '--input',
@@ -138,11 +123,10 @@ def main():
                         action='store',
                         help='Files that will be hashed')
 
-    # if defined a filename here, show to user that -d can be used to only show name of variables
     parser.add_argument('-o',
                         '--output',
                         metavar='DIR',
-                        type=is_folder,
+                        type=is_dir,
                         action='store',
                         help='Location were hashed files will be stored')
 
@@ -184,8 +168,7 @@ def main():
     # output_file_name      = output.txt
     # output_file_name_only	= output
     # preserve_file_path    = /pre/input.txt
-    # input_file_list       = input1.txt
-    #                         input2.txt
+    # input_file_list       = input1.txt, input2.txt
     output_file_basepath = output_folder + "/"
     input_file_basepath = input_folder + "/"
 
@@ -194,7 +177,9 @@ def main():
         # "/in/" + "input.txt" -> "/in/input.txt"
         input_file_path = input_file_basepath + input_file_name
 
-        if input_file_name == "rename.py":  # todo: sys.argv[0]
+        # TODO: check if input_file_name == argv[0]
+        # argv[0] == 'python rename.py' || './renamy.py' || ...
+        if input_file_name == "rename.py":
             print("Skipping source file " + input_file_name)
             continue
 
@@ -211,12 +196,12 @@ def main():
         # only hash_string without path, name or extensions
         # return("01234567890abcdef01234567890abcd")
         output_file_name_only = hash_file(input_file_path, use_hash)
-        # output_file_name = "01234567890abcdef01234567890abcd" + "txt" <- "/in/input.txt"
+        # output_file_name = "01234567890abcdef01234567890abcd" + ".txt"
         output_file_name = output_file_name_only + \
             os.path.splitext(input_file_path)[1]
         output_file_path = output_file_basepath + output_file_name
 
-        action_move(input_file_path, output_file_path, dry_run)
+        try_move(input_file_path, output_file_path, dry_run)
 
 
 if __name__ == "__main__":
