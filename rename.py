c02a251117ccd6c4ed7d5254f118ccd97c1a991a
mateusjdev <mateusjuniordev@gmail.com>
2025-01-05 23:28:21 -0300

    def is_git_dir

--- rename.py
+++ rename.py
@@ -7,7 +7,7 @@ import string
 import abc
 from random import choices
 from shutil import which
-from subprocess import call, STDOUT
+from subprocess import call, DEVNULL
 
 class RenameAlgorithm:
     """List of Hashing Algorithms"""
@@ -346,6 +346,22 @@ def is_dir(path: str):
 
     exit_with_error(f"Not a valid directory: '{path}'", ErrorExitCode.USER_ERROR)
 
+
+def is_git_dir(path: str) -> bool:
+    # ignore check if --input is a file
+    if not is_dir(path):
+        return False
+
+    # TODO: Improve .git repo detection
+    if which("git"):
+        if call(["git", "-C", os.path.normpath(path), "rev-parse"], stderr=DEVNULL, stdout=DEVNULL) == 0:
+            return True
+    # TODO: walk directory sctructure
+    elif os.path.exists(os.path.join(path, ".git")):
+        return True
+
+    return False
+
 def main():
     parser = argparse.ArgumentParser(
         description="Single python file to rename all files in a directory to \
@@ -440,14 +456,8 @@ def main():
     if argsp.dry_run or argsp.debug:
         logger.info(vars(argsp))
 
-    # TODO: Improve .git repo detection
-    if os.path.isdir(argsp.input):
-        if which("git") and not argsp.debug: # ignore (used for debudding)
-            if call(["git", "-C", os.path.normpath(argsp.input), "rev-parse"], stderr=STDOUT, \
-                    stdout=open(os.devnull, 'w', encoding='UTF-8')) == 0:
-                exit_with_error("Input path is git repo!", ErrorExitCode.SOFT_ERROR)
-        elif os.path.exists(os.path.join(argsp.input, ".git")):
-            exit_with_error("Input path is git repo!", ErrorExitCode.SOFT_ERROR)
+    if is_git_dir(argsp.input) and not argsp.debug: # ignore (used for debudding):
+        exit_with_error("Input path is git repo!", ErrorExitCode.SOFT_ERROR)
 
     rename_helper = RandomRenameHelper(argsp.dry_run, logger,argsp.recursive, argsp.verbose,
         argsp.lenght, argsp.uppercase) if argsp.hash == RenameAlgorithm.FUZZY else \
